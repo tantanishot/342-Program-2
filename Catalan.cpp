1257cd74b033f557bd00dc6d30a9b52d8a6d1347
tantanishot <132625622+tantanishot@users.noreply.github.com>
2024-02-01 00:27:52 -0800

    Add files via upload

--- Catalan.cpp
+++ Catalan.cpp
@@ -1,23 +1,43 @@
+//Catalan class that calculates using the 
+//recursive formula computing the nth number
+
 #include <iostream>
 using namespace std;
 
+
+//Catalan method. This is where mainly the 
+//Code computes through recursion until basecase hits
+//1
  int Catalan(int n){
-    int total = 0;
     if(n <= 1) 
     {
         return 1;
     }
     else 
     {
-    for(int i = 0; i < n; i++) {
-        total += Catalan(n) * Catalan(n - i);
+        long long int total = 0;
+        for(int i = 0; i < n; i++) {
+            total += Catalan(i) * Catalan(n - i - 1);
+        }
+        return total;
     }
-    }
- 
-    return total;
 }
 
-int main(){
-    cout << Catalan(10) << endl;
+//Main method which gets an integer from the terminal
+//and stores it in int n. Checks if negative #'s pass through
+//which are not allowed
+int main(int argc, char* argv[]){
+    //atoi transfers character into integer and uses 
+    //the first 2nd index argument as the first index (0)
+    //is the exe file
+    int n = atoi(argv[1]);
+    if(n < 0) 
+    {   
+        cout << "Cant Catalan a negative number.";
+    }
+    else 
+    {
+        cout << "catalan " << n << " " << endl << Catalan(n) << endl;
+    }
     return 0;
-};
+}
